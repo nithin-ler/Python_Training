531a1a598ed2be628e801d0c498d8b407c77a9ab
nithin-ler <fixed-term.nithin.kshine@bosch.com>
2025-08-29 17:11:26 +0530

    First commit

---
+++ sysargv.py
@@ -0,0 +1,15 @@
+# *args -variable no of arguments
+# **kwargs-key value pair
+ 
+#  sys.argv is a list that contains command-line arguments passed to a Python script.
+ 
+import sys
+tot=0
+k=len(sys.argv)
+print("Size  =" , k)
+print(sys.argv[0])
+ 
+for i in range(1,k):
+    tot=tot+ int(sys.argv[i])
+ 
+print("Total  =" , tot)
